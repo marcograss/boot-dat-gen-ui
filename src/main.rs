f98a9dfc0068cf691c53f3767ac6c529c642ca1d
Marco Grassi <marco.gra@gmail.com>
2023-09-24 18:12:57 +0200

    first working version

---
+++ src/main.rs
@@ -0,0 +1,139 @@
+use std::collections::HashMap;
+
+use base64::{engine::general_purpose, Engine as _};
+use gloo::file::callbacks::FileReader;
+use gloo::file::File;
+use web_sys::{DragEvent, Event, FileList, HtmlInputElement};
+use yew::html::TargetCast;
+use yew::{html, Callback, Component, Context, Html};
+
+struct FileDetails {
+    name: String,
+    _data: Vec<u8>,
+    converted: Vec<u8>,
+}
+
+pub enum Msg {
+    Loaded(String, Vec<u8>),
+    Files(Vec<File>),
+}
+
+pub struct App {
+    readers: HashMap<String, FileReader>,
+    files: Vec<FileDetails>,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            readers: HashMap::default(),
+            files: Vec::default(),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Loaded(file_name, data) => {
+                let converted =
+                    tx_custom_boot::generate_boot_dat(&data).expect("cannot generate payload");
+                self.files.push(FileDetails {
+                    _data: data,
+                    name: file_name.clone(),
+                    converted,
+                });
+                self.readers.remove(&file_name);
+                true
+            }
+            Msg::Files(files) => {
+                for file in files.into_iter() {
+                    let file_name = file.name();
+
+                    let task = {
+                        let link = ctx.link().clone();
+                        let file_name = file_name.clone();
+
+                        gloo::file::callbacks::read_as_bytes(&file, move |res| {
+                            link.send_message(Msg::Loaded(
+                                file_name,
+                                res.expect("failed to read file"),
+                            ))
+                        })
+                    };
+                    self.readers.insert(file_name, task);
+                }
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div id="wrapper">
+                <p id="title">{ "Generate TX boot.dat from payload.bin" }</p>
+                <label for="file-upload">
+                    <div
+                        id="drop-container"
+                        ondrop={ctx.link().callback(|event: DragEvent| {
+                            event.prevent_default();
+                            let files = event.data_transfer().unwrap().files();
+                            Self::upload_files(files)
+                        })}
+                        ondragover={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                        ondragenter={Callback::from(|event: DragEvent| {
+                            event.prevent_default();
+                        })}
+                    >
+                        <i class="fa fa-cloud-upload"></i>
+                        <p>{"Drop your payload.bin here or click to select"}</p>
+                    </div>
+                </label>
+                <input
+                    id="file-upload"
+                    type="file"
+                    multiple={true}
+                    onchange={ctx.link().callback(move |e: Event| {
+                        let input: HtmlInputElement = e.target_unchecked_into();
+                        Self::upload_files(input.files())
+                    })}
+                />
+                <div id="preview-area">
+                    { for self.files.iter().map(Self::view_file) }
+                </div>
+            </div>
+        }
+    }
+}
+
+impl App {
+    fn view_file(file: &FileDetails) -> Html {
+        html! {
+            <div class="preview-tile">
+                // <p class="preview-name">{ format!("Converted boot.dat for {}", file.name) }</p>
+                <a class="preview-name" href={format!("data:image/jpg;base64,{}", general_purpose::STANDARD_NO_PAD.encode(&file.converted))} download="boot.dat">{ format!("Converted boot.dat for {}", file.name) }</a>
+            </div>
+        }
+    }
+
+    fn upload_files(files: Option<FileList>) -> Msg {
+        let mut result = Vec::new();
+
+        if let Some(files) = files {
+            let files = js_sys::try_iter(&files)
+                .unwrap()
+                .unwrap()
+                .map(|v| web_sys::File::from(v.unwrap()))
+                .map(File::from);
+            result.extend(files);
+        }
+        Msg::Files(result)
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
